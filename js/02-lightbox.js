77c6aa72c30bea947b7a9b811aaf31bf98ea2f01
Yukhnenko Nataliia <101944838+Natali2721@users.noreply.github.com>
2022-08-21 15:16:40 +0300

    02

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,36 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+//console.log(galleryItems);
+
+const galleryContainer = document.querySelector(".gallery");
+const galleryItemsMarkup = createGalleryItemsMarkup(galleryItems);
+
+//var1 якщо елемент пустий, або переписується наново весь
+//galleryContainer.innerHTML = galleryItemsMarkup;
+galleryContainer.insertAdjacentHTML("beforeend", galleryItemsMarkup); // додали в розмітку
+
+function createGalleryItemsMarkup(images) {
+  return images
+    .map(({ preview, original, description }) => {
+      return `
+  <a class=class="gallery__item" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a>
+`;
+    })
+    .join("");
+}
+
+let gallery = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: "250",
+});
+
+gallery.on("show.simplelightbox", function () {});
+
+const slideDescr = document.querySelector(".sl-caption");
